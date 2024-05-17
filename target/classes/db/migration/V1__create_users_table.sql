eaceede55aedbafb78a6d16a51951f8d143ff7e1
salita_dima <salitadima1@gmail.com>
2024-05-17 10:18:07 +0200

    Update Setting

--- target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE t_user(
-                      id SERIAL PRIMARY KEY,
-                      firstname VARCHAR(255),
-                      lastname VARCHAR(255) NOT NULL,
-                      email VARCHAR(255) NOT NULL UNIQUE,
-                      password VARCHAR NOT NULL
-);
